2a0fa01b8bc5fa3c92d75a02d206376c8148eeaf
TonKlaTonKub <phetkla1992@gmail.com>
2021-12-29 21:34:05 +0700

    POST Data to Databas

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ContactMessage
 
 # Create your views here.
 def Home(request):
@@ -9,4 +10,17 @@ def Service(request):
     return render(request, 'home/service.html')
 
 def Contact(request):
+    if request.method == 'POST':
+        data = request.POST.copy()
+        title = data.get('title')
+        email = data.get('email')
+        detail = data.get('detail')
+        print(title, email, detail)
+        
+        new = ContactMessage()
+        new.title = title
+        new.email = email
+        new.detail = detail
+        new.save()
+
     return render(request, 'home/contact.html')
